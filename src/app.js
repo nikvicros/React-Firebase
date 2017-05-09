c6a8b1b08478a7acc4fc194bf7d8de5ab1bddb2b
Nikho Rosales <nikho.rosales@emilys-air.attlocal.net>
2017-05-09 16:01:10 -0500

    Firebase Init code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
 import React, {Component}from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 
 //Custom Styles
 import { Header } from './components/common';
 
+//Firebase Initiation
+
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyBPRxHCJehsKf9_AzZseVkkylIwAZSvkvA",
+            authDomain: "react-native-authenticate.firebaseapp.com",
+            databaseURL: "https://react-native-authenticate.firebaseio.com",
+            projectId: "react-native-authenticate",
+            storageBucket: "react-native-authenticate.appspot.com",
+            messagingSenderId: "837649889212"
+        });
+    }
+
+    
     render() {
         return (
             <View>
-                <Header headerText="Please Login" />
+                <Header headerText="Please Login"/>
                 <Text>App</Text>
             </View>
         );
