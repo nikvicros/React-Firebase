94f2818085adab7c06b156217f73f7f8546be22c
Nikho Rosales <nikho.rosales@emilys-air.attlocal.net>
2017-05-09 14:51:17 -0500

    Allow Android and iOS development

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Root Application</Text>
+            </View>
+        );
+    };
+};
